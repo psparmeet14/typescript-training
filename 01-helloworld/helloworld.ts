5020dd3428170905d318c30e614fe6dd7b08d058
Parmeet Singh <psparmeet14@gmail.com>
2022-12-12 20:49:39 +0530

    ts variables practice

--- 01-helloworld/helloworld.ts
+++ 01-helloworld/helloworld.ts
@@ -1,1 +1,3 @@
 console.log("Hello world!");
+console.log("Coding Gurus! " + "Time for some fun!");
+
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,15 @@
+let found: boolean = false;
+let grade: number = 88.6;
+let firstName: string = "Parmeet";
+let lastName: string = "Singh";
+let myData: any = 50.0;
+myData = false;
+myData = "Eric";
+myData = 10;
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// Template Strings
+console.log(`Template String: Hi ${firstName} ${lastName}`);
